198df549fa571cb39b22b0e8611ad58056c50aab
Wendy Miller <weendymilleer@gmail.com>
2024-07-28 15:44:01 -0300

    Modified AppConfig.java

--- src/main/java/com/picpaysimplificado/domain/infra/AppConfig.java
+++ src/main/java/com/picpaysimplificado/domain/infra/AppConfig.java
@@ -1,2 +1,15 @@
-package com.picpaysimplificado.domain.infra;public class AppConfig {
+package com.picpaysimplificado.domain.infra;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
